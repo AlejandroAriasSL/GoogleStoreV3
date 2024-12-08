2b8c5f5f161be0f92b867a6272f5a09b3818d91b
erikamc99 <erikamc99@gmail.com>
2024-12-08 18:33:13 +0100

    form cod postal tablet

--- GoogleStore/src/views/Tablet/app.js
+++ GoogleStore/src/views/Tablet/app.js
@@ -37,3 +37,14 @@ fetch('/src/views/img')
       .catch(error => {
           console.error(error);
       });
+
+      const form = document.getElementById('postalCodeForm');                        // Añadimos un evento 'submit' al formulario
+      form.addEventListener('submit', (event) => {
+        // Evitamos el comportamiento predeterminado del formulario (recargar la página)
+        event.preventDefault();                            // Obtenemos el valor del código postal
+        const postalCode = document.getElementById('postalCode').value;                            // Validamos o procesamos el código postal
+        if (postalCode.trim() === '') {
+            document.getElementById('output').textContent = 'Por favor, ingresa un código postal.';
+        }
+    });
+   
